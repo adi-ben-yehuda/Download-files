45c5da5ea0df8a9ab2e26e9394fe09f973eb98a8
adi-ben-yehuda <adiby2017@gmail.com>
2022-12-08 18:51:08 +0200

    fix the code

--- tcp_server.py
+++ tcp_server.py
@@ -5,9 +5,10 @@ import os
 ## Get the name of the file.
 def getFileName(data):
     fileName = data.split(' ')[1]
-    ## Check if the name file is '/'- we want the index.html file.
+    ## If the file name is '/', change the file name to be index.html.
     if fileName == '/':
         fileName = "/index.html"
+
     return fileName
 
 ## Create the message to send to the client.
@@ -26,7 +27,7 @@ def messageToClient(data):
     
     ## Check if the file is in the directory.
     elif(fileExist(fileName)):
-        ## If the file name is the directory
+        ## If the file name is in the directory
         status += " 200 OK"
         connection = data.split('\n')[2].split('\r')[0]
         size = os.path.getsize(path)
@@ -42,42 +43,45 @@ def messageToClient(data):
 
     return finalMess
         
-
-# Check if file exist in the directory.
+## Check if file exists in the directory.
 def fileExist(fileName):
     path = 'files/files' + fileName
     return os.path.exists(path)
 
+## Open the specific file according to the name and return it.
 def openFile(fileName):
     path = 'files/files' + fileName
     file = None
+
     if(fileExist(fileName)):
         file = open(path, 'rb')
+
     return file
 
 
 ## Get data from the command line and check if valid.
 args = sys.argv
-#if(len(args) == 2 and args[1].isnumeric() and (int)(args[1]) in range(0, 65535)):
-#  port = (int)(args[1])
+if(len(args) == 2 and args[1].isnumeric() and (int)(args[1]) in range(0, 65535)):
+    port = (int)(args[1])
 
 ## Create socket.
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('', 8080))
+server.bind(('', port))
 server.listen(5)
 client_socket, client_address = server.accept()
 client_socket.settimeout(1)
 
-
 while True:
-    print("here")
     try:
         data = client_socket.recv(4096).decode("utf-8")
+        ## If the requwst is empty, close the connection and wait for the next client.
         if data == '':
             client_socket.close()
             client_socket, client_address = server.accept()
             client_socket.settimeout(1)
         else:
+            # If the end of the message is '\r\n\r\n', 
+            # it means the client stops sending the message.
             while not data.endswith('\r\n\r\n'):
                 data += client_socket.recv(4096).decode("utf-8")
 
@@ -85,8 +89,8 @@ while True:
 
             message = messageToClient(data)
             client_socket.send(message)
-            fileName = getFileName(data)
 
+            fileName = getFileName(data)
             if fileName == '/redirect':
                 fileName = '/result.html'
 
@@ -95,19 +99,18 @@ while True:
                 client_socket.sendfile(file)
                 file.close()
                         
-            
-            ## Check if the client ask to close the conncection.
             connection = data.split('\n')[2].split('\r')[0]
+            ## Check if the client asks for closing the conncection.
             if connection == "Connection: close":
                 client_socket.close()
-            ## If the connection isn't a keep alive, 
+            # If the connection isn't a keep alive, 
             # close the connection and open a new socket.
             elif connection != "Connection: keep-alive":
                 client_socket.close()
                 client_socket, client_address = server.accept()
                 client_socket.settimeout(1)
-    except:
+    except: 
+        # That is, there is a timeout.
         client_socket.close()
         client_socket, client_address = server.accept()
         client_socket.settimeout(1)
-
